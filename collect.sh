60c7a0eebc3f3cf204348c62df90e9af58e12ad8
Mykola Golub <mgolub@mirantis.com>
2015-04-27 14:10:20 +0300

    collect.sh: collect stats from all ceph hosts

---
+++ collect.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+set -e
+
+#
+# Globals
+#
+
+: ${CEPH_SCRIPTS_DIR:=$(readlink -f $(dirname $0))}
+: ${CEPH_DATA_DIR:=${CEPH_SCRIPTS_DIR}/data}
+
+DATE=$(date '+%F')
+
+#
+# Functions
+#
+
+list_ips()
+{
+    (
+	for id in `ceph osd ls`
+	do 
+	    ceph osd find ${id} |
+	    sed -nEe 's/^.*"ip": "([^:]*):.*/\1/p'
+	done
+	ceph mon dump 2>/dev/null |
+	sed -nEe 's/^[0-9]*: ([^:]*):.*$/\1/p'
+    ) | sort -u
+}
+
+is_my_ip()
+{
+    local ip=$1
+
+    ip address list | fgrep -q "inet ${ip}/"
+}
+
+prepare()
+{
+    mkdir -p "${CEPH_DATA_DIR}"
+
+    for ip in $(list_ips)
+    do
+	if ! is_my_ip ${ip}
+	then
+	    deploy_scripts ${ip}
+	fi
+    done
+
+    trap "pkill -P $$" INT TERM
+}
+
+deploy_scripts()
+{
+    local ip=$1
+
+    tar -C / -cf - "${CEPH_SCRIPTS_DIR}" |
+    ssh ${ip} tar -C / -xf -
+}
+
+collect_host_stats()
+{
+    local ip=$1; shift
+
+    mkdir -p "${CEPH_DATA_DIR}"/${ip}
+    ssh ${ip} ${CEPH_SCRIPTS_DIR}/host-stats/collect.sh "$@"
+    scp ${ip}:"/var/log/ceph/ceph-stats.*.${DATE}.log" "${CEPH_DATA_DIR}/${ip}"
+}
+
+collect_ceph_stats()
+{
+    ${CEPH_SCRIPTS_DIR}/ceph-stats/collect.sh "$@"
+    cp /var/log/ceph/ceph-stats.${DATE}.log "${CEPH_DATA_DIR}"
+}
+
+collect()
+{
+    for ip in $(list_ips)
+    do
+	collect_host_stats ${ip} "$@"&
+    done
+
+    collect_ceph_stats "$@"&
+
+    wait
+}
+
+make_tarball()
+{
+    local rootdir=$(dirname "${CEPH_DATA_DIR}")
+    local datadir=$(basename "${CEPH_DATA_DIR}")
+
+    tar -C "${rootdir}" -czf "${datadir}.tar.gz" "${datadir}"
+}
+
+
+#
+# Main
+#
+
+prepare
+collect "$@"
+make_tarball
