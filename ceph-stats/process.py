7f307a7e6b8db3c37fc3a5f9eb4eb086d125cbde
Mykola Golub <mgolub@mirantis.com>
2015-05-07 13:00:39 +0300

    Add --json-pretty option.

--- ceph-stats/process.py
+++ ceph-stats/process.py
@@ -36,13 +36,20 @@ def parse_args():
         default=CEPHSTATS_DATE,
         )
     parser.add_argument(
+        '-p', '--json-pretty',
+        action='store_true',
+        default=False,
+        help='json-prettify output',
+        )
+    parser.add_argument(
         'name',
         help='statistics name to look for',
         )
     parser.add_argument(
         'key',
-        nargs='+',
+        nargs='*',
         help='key in statistics to look for',
+        default=['']
         )
     args = parser.parse_args()
     return args
@@ -54,7 +61,8 @@ def main():
     ctx = parse_args()
     f = open(logfile(ctx.date), 'r')
     r = re.compile('^(\d\d\d\d-\d\d-\d\d \d\d:\d\d:\d\d) \[%s\] \s*(.*)$' % (ctx.name))
-    print '#"date" "time"', ' '.join(['"' + x + '"' for x in ctx.key])
+    if not ctx.json_pretty:
+        print '#"date" "time"', ' '.join(['"' + x + '"' for x in ctx.key])
     for line in f:
         m = r.match(line)
         if not m:
@@ -62,6 +70,8 @@ def main():
         t = m.group(1)
         val = json.loads(m.group(2))
         print t,
+        if ctx.json_pretty:
+            print ctx.name
         for key in ctx.key:
             v = val
             for k in key.split():
@@ -72,7 +82,10 @@ def main():
                 except:
                     v = None
                     break
-            print v is None and '-' or v,
+            if ctx.json_pretty:
+                print key, ' = ', json.dumps(v, sort_keys=True, indent=4, separators=(',', ': '))
+            else:
+                print v is None and '-' or v,
         print
 
 main()
