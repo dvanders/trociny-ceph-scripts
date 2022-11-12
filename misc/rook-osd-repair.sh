ee0aafb15559379384f245af9aba702374ab5b2f
Mykola Golub <to.my.trociny@gmail.com>
2022-11-12 17:58:33 +0000

    rook-osd-repair: some other small improvements
    Signed-off-by: Mykola Golub <to.my.trociny@gmail.com>

--- misc/rook-osd-repair.sh
+++ misc/rook-osd-repair.sh
@@ -21,9 +21,6 @@
 #      --data-path /var/lib/ceph/osd/ceph-0 --op list-pgs
 #
 
-# For testing
-# alias kubectl="echo kubectl"
-
 #
 # Globals
 #
@@ -36,6 +33,13 @@ NAMESPACE=rook-ceph
 
 usage() {
     echo "usage: $0 <osd-id>" >&2
+}
+
+get_osd_pod_name() {
+    local id=$1
+
+    kubectl -n rook-ceph get pods | grep "^rook-ceph-osd-${id}-" |
+	awk '{print $1}'
 }
 
 backup_deployment() {
@@ -53,22 +57,37 @@ restore_deployment() {
 
 patch_deployment() {
     local id=$1
-    local i pod
+    local i pod host_ip
 
     kubectl -n ${NAMESPACE} patch deployment rook-ceph-osd-${id} --type='json' \
 	    -p '[{"op":"remove", "path":"/spec/template/spec/containers/0/livenessProbe"}]'
     kubectl -n ${NAMESPACE} patch deployment rook-ceph-osd-${id} \
 	    -p '{"spec": {"template": {"spec": {"containers": [{"name": "osd", "command": ["sleep", "infinity"], "args": []}]}}}}'
 
-    # wait for restart complete
     kubectl -n ${NAMESPACE} wait --for=condition=Progressing deployment/rook-ceph-osd-${id}
+
+    echo -n "waiting for pod " >&2
     for i in `seq 60`; do
+	echo -n "." >&2
 	kubectl -n rook-ceph exec -it deploy/rook-ceph-osd-${id} -- \
-		ps auxww 2>/dev/null | grep -q 'sleep infinity' && return
+		ps auxww 2>/dev/null | grep -q 'sleep infinity' && break
 	sleep 1
     done
+    echo >&2
 
-    return 1
+    kubectl -n rook-ceph exec -it deploy/rook-ceph-osd-${id} -- \
+	    ps auxww 2>/dev/null | grep -q 'sleep infinity'
+
+    pod=$(get_osd_pod_name ${id})
+    test -n "${pod}"
+
+    host_ip=$(kubectl -n rook-ceph get pod ${pod} -o jsonpath='{.status.hostIP}')
+
+    host_path=$(kubectl -n rook-ceph get pod ${pod} -o json |
+                jq -r '.spec.volumes[] | select(.name == "rook-ceph-log").hostPath.path')
+
+    echo "${pod} pod started" >&2
+    echo "CWD on the host: ${host_ip}:${host_path}" >&2
 }
 
 pod_exec() {
